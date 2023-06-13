dfaa682887d603f868577921d7a4ea062e4716e7
irene hernandez <hernandez.o.irene@gmail.com>
2023-06-13 20:10:44 +0800

    completed

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks#, dependent: :destroy this is wrong, if smb deletes a list, should not destroy all bookmarks from everybody
   validates :name, uniqueness: true
   validates :name, presence: true
+  has_one_attached :photo
 end
